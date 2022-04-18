18666f05b59c09dff8a0e539afc6a0b219e8540e
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-18 14:15:18 +0300

    add: spring boot with spring-boot (https://start.spring.io/)

---
+++ src/test/java/by/paramonov/warehouse/WarehouseOfPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package by.paramonov.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseOfPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
